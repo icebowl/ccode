707ad69bc62eebc74949758fdf13bd7056c502f1
icebowl <icebowl1967@gmail.com>
2019-01-04 06:18:43 -0800

    AA

---
+++ codecademy/switch_planets.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+  
+  double earthw, planetw, ratio;
+ int planet;
+  // 	Planet 	Relative Gravity
+    swtich(planet){
+    case 1:
+      std::cout <<"Venus\n";
+			ratio = 0.78;
+      break;
+    case 2:
+      std::cout<<"Mars\n";
+      ratio = 0.39;
+      break;
+    case 3:
+      
+3 	Jupiter 	2.65
+4 	Saturn 	1.17
+5 	Uranus 	1.05
+6 	Neptune 	1.23
+
+
+/*
+# 	Planet 	Relative Gravity
+1 	Venus 	0.78
+2 	Mars 	0.39
+3 	Jupiter 	2.65
+4 	Saturn 	1.17
+5 	Uranus 	1.05
+6 	Neptune 	1.23
+
+*/
