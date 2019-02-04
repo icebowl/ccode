b1a88f41a1e147dc84e57e95a0a3d69346099134
icebowl <sift@owt.com>
2019-02-04 13:53:03 -0800

    helloworld.c

---
+++ c/helloworld.c
@@ -0,0 +1,13 @@
+// hello.c
+// compile gcc hello.c
+// run ./a.out
+#include <stdio.h>
+int main(){
+  printf("**** Hello World ****\n");
+  printf("*       from        *\n");
+  printf("*  CCC W     W  CCC *\n");
+  printf("* C     W W W  C    *\n");
+  printf("*  C      W W   C   *\n");
+  printf("*********************\n");
+  return 0;
+}
