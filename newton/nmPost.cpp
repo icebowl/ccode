39364ff5eedb7f6c1a49cc5c70fa3601261547f0
icebowl <icebowl1967@gmail.com>
2019-06-06 20:04:13 -0700

    AA

--- newton/nmPost.cpp
+++ newton/nmPost.cpp
@@ -1,34 +1,36 @@
-/* structs.c: study the struct in C */
+
 #include <iostream>
-#include <array>
+
 using namespace std;
+
 struct ABi {
    double a;
    double bi;
 };
 
-void printABi(struct ABi abi);
-ABi abiPower (struct ABi abi, int root);
-ABi abiDivide (struct ABi num, ABi den);
+void printABi(ABi abi);
+ABi abiPower (ABi abi, int root);
+ABi abiDivide (ABi num, ABi den);
 
 // main()
 int main() {
-   ABi xyin, xyin1, quo, num, den;
+   ABi xyin, quo, num, den;
    xyin.a = -2.0;
    xyin.bi = 2.0;
-   printABi(xyin);
-   //xyin1 = abiPower(xyin,4);
-   cout<<" print xyin1 power  "<<endl;
-   //printABi(xyin1);
+ 
+   cout<<" print xyin 1"<<endl;
   // printABi(xyin);
-   num = abiPower(xyin,4);
+    num = abiPower(xyin,4);
+  cout<<" print xyin 2"<<endl;
+//   printABi(xyin);
    den = abiPower(xyin,4);
+   
    cout<<" print num den  "<<endl;
-  // printABi(num);
- //  printABi(den);
+  // printABi(den);
+ //  printABi(num);
    quo = abiDivide(num,den);
    cout<<" quotient "<<endl;
-   printABi(xyin);
+  // printABi(xyin);
    printABi(quo);
 
 }
@@ -38,7 +40,7 @@ int main() {
 void printABi(ABi abi) {
   double x = abi.a;
   double yi = abi.bi;
-   cout <<"* * * print a+bi * * * \n"<< "a+bi = "<<x<<" + "<<yi<<"i \n * * * * * * * \n"<<endl;
+   cout <<"* * * print a+bi -> "<< x<<" + "<<yi<<" i "<<endl;
 }
 
 ABi abiPower (ABi abi, int root){
@@ -47,22 +49,23 @@ ABi abiPower (ABi abi, int root){
   double bi = abi.bi;
   double xt, yti,x,yi;
   int n;
-//  cout<<"a "<<a<<" bi "<<bi<<"\n";
-//  cout<<" * * x "<<x<<" yi "<<yi<<"\n";
+  x = a; yi = bi;
   // a+bi and x+yi loop for products
   for (n = 1; n < root; n++){
     xt = (x*a)-(yi*bi);
     yti = (a*yi)+(x*bi);
     x = xt; yi = yti;
-    //cout<<" * * nx "<<xt<<" yi "<<yti<<"\n";
   }
-  //cout<<" * * x "<<x<<" yi "<<yi<<"\n";
+   cout<<" abiPower "<<endl;
+  cout << " x " << x << " yi "<< yi <<endl;
   abiReturn.a = x;
   abiReturn.bi = yi;
+ 
+  printABi(abiReturn);
   return abiReturn;
 }
 
-ABi abiDivide (ABi n, struct ABi d){
+ABi abiDivide (ABi n, ABi d){
   ABi quo;
   double a = n.a;
   double bi = n.bi;
@@ -70,9 +73,6 @@ ABi abiDivide (ABi n, struct ABi d){
   double yi = d.bi;
   double hn,kni ,kd,q,qi;
   // numberator
-  cout<<" @ @ @ "<<endl;
-  printABi(n);
-  printABi(d);
   hn = (a*x + bi * yi);
   kni = (-1*a*yi + x * bi) ;
   //denominator
@@ -80,7 +80,5 @@ ABi abiDivide (ABi n, struct ABi d){
   //divide
   q = hn / kd;  qi = kni / kd;
   quo.a = q; quo.bi = qi;
-  cout<<" ~ ~ ~ "<<endl;
-  printABi(quo);
   return quo;
 }
